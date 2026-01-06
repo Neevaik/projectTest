85876802a9d0a43d59fcf4ec2a7fa670763b003c
floriancharpenet <florian.charpenet@gmail.com>
2026-01-06 21:45:39 +0100

    Exercice fait

--- mon-app/src/TestPage.js
+++ mon-app/src/TestPage.js
@@ -1,16 +1,52 @@
+import { useState } from "react";
+
 function TestPage({ goToHome }) {
+    const [count, setCount] = useState(0);
+
     return (
         <div>
             <button onClick={goToHome}>Retour à Home</button>
 
-            <div>
+            <div style={styles.parent}>
               {/* 1ere etape faire le design de la page */}
+              <h1> Test  </h1>
 
-              
+              <p>Appuyez sur - ou + </p>  
+
+                <div>
+
+                </div>
+
+              <div style={styles.counter}>
+          <button onClick={() => setCount(count - 1)}>-</button>
+          <span style={styles.value}>{count}</span>
+          <button onClick={() => setCount(count + 1)}>+</button>
+              </div>
+
             </div>
 
         </div>
     );
 }
 
+const styles = {
+        parent: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "400px",
+        height: "300px",
+        backgroundColor: "#add8e6",
+        border: "3px solid blue",
+        padding: "20px",
+        margin: "50px",
+    },
+      counter: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "20px",
+      },
+}  
 export default TestPage;
