01e993d0554208bee504256dce933682d6be7705
Neevaik <kevin.mavier@hotmail.com>
2026-01-06 22:03:19 +0100

    exercice corrected

--- mon-app/src/TestPage.js
+++ mon-app/src/TestPage.js
@@ -1,52 +1,41 @@
-import { useState } from "react";
 
 function TestPage({ goToHome }) {
-    const [count, setCount] = useState(0);
 
     return (
         <div>
             <button onClick={goToHome}>Retour à Home</button>
-
             <div style={styles.parent}>
-              {/* 1ere etape faire le design de la page */}
-              <h1> Test  </h1>
-
-              <p>Appuyez sur - ou + </p>  
-
+                <h1>Test</h1>
+                <p style={styles.secondTitle}>Appuyez sur - ou + </p>
                 <div>
-
                 </div>
-
-              <div style={styles.counter}>
-          <button onClick={() => setCount(count - 1)}>-</button>
-          <span style={styles.value}>{count}</span>
-          <button onClick={() => setCount(count + 1)}>+</button>
-              </div>
-
+                <div style={styles.counter}>
+                    <span>-</span>
+                    <span style={styles.value}>0</span>
+                    <span>+</span>
+                </div>
             </div>
-
         </div>
     );
 }
 
 const styles = {
-        parent: {
+    parent: {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        width: "400px",
-        height: "300px",
-        backgroundColor: "#add8e6",
-        border: "3px solid blue",
         padding: "20px",
         margin: "50px",
     },
-      counter: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: "20px",
-      },
-}  
+    counter: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "20px",
+    },
+    secondTitle: {
+        textDecoration: "underline",
+    }
+}
 export default TestPage;
