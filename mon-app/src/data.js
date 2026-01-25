1df66c2e838c12a58dd7ad1961e8284a7d1f2c83
Neevaik <kevin.mavier@hotmail.com>
2026-01-25 18:29:15 +0100

    send exercice

---
+++ mon-app/src/data.js
@@ -0,0 +1,38 @@
+const users = [
+  {
+    name: "Alice",
+    age: 25,
+    isOnline: true,
+    avatar: "https://randomuser.me/api/portraits/women/44.jpg"
+  },
+  {
+    name: "Bob",
+    age: 30,
+    isOnline: false,
+    avatar: "https://randomuser.me/api/portraits/men/35.jpg"
+  },
+  {
+    name: "Charlie",
+    age: 22,
+    isOnline: true,
+    avatar: "https://randomuser.me/api/portraits/men/32.jpg"
+  },
+    {
+    name: "Paul",
+    age: 33,
+    isOnline: true,
+    avatar: "https://randomuser.me/api/portraits/men/33.jpg"
+  },
+   {
+    name: "Tom",
+    age: 44,
+    isOnline: false,
+    avatar: "https://randomuser.me/api/portraits/men/44.jpg"
+  },
+   {
+    name: "Lisa",
+    age: 19,
+    isOnline: true,
+    avatar: "https://randomuser.me/api/portraits/men/19.jpg"
+  },
+];
