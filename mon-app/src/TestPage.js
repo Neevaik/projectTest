e68e18a0161b35d7ca608ddcd29caefb86520d32
Neevaik <kevin.mavier@hotmail.com>
2026-02-01 21:36:24 +0100

    exo corrected

--- mon-app/src/TestPage.js
+++ mon-app/src/TestPage.js
@@ -1,29 +1,25 @@
 import Navbar from "./components/Navbar";
-import Styles from "./css/Styles.css";
-import Movies from "./components/Movies";
-import { movies } from '../src/data'
-import { watchedMovies } from '../src/data'
+import { movies } from '../src/data/movies'
+import { watchedMovies } from '../src/data/watchedMovies'
+import MovieCard from "./components/MovieCard";
 
 function TestPage({ goToExercice, goToHome, goToTest2 }) {
 
-        const watchedOnly = movies.filter(movie =>
-  watchedMovies.some(w => w.id === movie.id)
-);
-    return (
-        <div>
-            <Navbar title={"Page test"} goToExercice={goToExercice} goToHome={goToHome} goToTest={() => { }} goToTest2={goToTest2} />
-            <div style={Styles.parent}>
-                <h1>Films</h1>
-                <div>
-                </div>
-                <div style={Styles.styles}>
+    const notWatched = movies.filter(x => !watchedMovies.some(y => x.id === y.id));
 
-                </div>
+    return (
+        <>
+            <div>
+                <Navbar title={"Page test"} goToExercice={goToExercice} goToHome={goToHome} goToTest={() => { }} goToTest2={goToTest2} />
             </div>
-        </div>
+
+            <div>
+                {notWatched.map(movie => (
+                    <MovieCard id={movie.id} title={movie.title} year={movie.year} />
+                ))}
+            </div>
+        </>
     );
 }
 
-
 export default TestPage;
-// test
---
+++ mon-app/src/components/MovieCard.js
@@ -0,0 +1,13 @@
+import "../css/MovieCard.css";
+
+function MovieCard({ id, title, year }) {
+  return (
+    <div className="movie-card">
+      <p className="movie-id">#{id}</p>
+      <p className="movie-title">{title}</p>
+      <p className="movie-year">{year}</p>
+    </div>
+  );
+}
+
+export default MovieCard;
