c2c068289f11ef195cd1eaca1f15facb9c1a5949
Neevaik <kevin.mavier@hotmail.com>
2026-02-02 10:06:37 +0100

    exo products

---
+++ mon-app/src/data/products.js
@@ -0,0 +1,6 @@
+export const products = [
+  { id: 1, name: "Clavier", price: 49, inStock: true, onSale: false },
+  { id: 2, name: "Souris", price: 29, inStock: true, onSale: true },
+  { id: 3, name: "Écran", price: 199, inStock: false, onSale: false },
+  { id: 4, name: "Casque", price: 89, inStock: true, onSale: true },
+];
